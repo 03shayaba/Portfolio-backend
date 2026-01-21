33936d25dd2cab63fa1983ba42c6e48381582fa7
03shayaba <03shayaba@users.noreply.github.com>
2026-01-21 10:18:20 +0530

    sendmail utils updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,28 @@
 const nodemailer = require("nodemailer");
-const sendEmail = async({name, email,   descriptionMessage}) =>{
-    try{
-        // Create a transporter
+const sendEmail = async ({ name, email, descriptionMessage }) => {
+  try {
+    // Create a transporter
 
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        })
+     const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // TLS
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
 
-        // Define email options
-        const mailOptions = {
-            from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
-replyTo: email
-,
-  to: process.env.EMAIL_USER,
-  subject: ` New Message from Portfolio`,
-  html: `
+    // Define email options
+    const mailOptions = {
+      from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
+      replyTo: email,
+      to: process.env.EMAIL_USER,
+      subject: ` New Message from Portfolio`,
+      html: `
     <div style="max-width:600px;margin:auto;font-family:Arial;">
       <h2 style="background:#4CAF50;color:white;padding:10px;">
         New Contact Form Submission
@@ -30,15 +34,14 @@ replyTo: email
         <p style="background:#f4f4f4;padding:10px;">${descriptionMessage}</p>
       </div>
     </div>
-  `
-        };
-        // Send email
+  `,
+    };
+    // Send email
 
     await transporter.sendMail(mailOptions);
-
-    }catch(err){
-        console.log("Error in sendEmail util", err);
-    }
-}
+  } catch (err) {
+    console.log("Error in sendEmail util", err);
+  }
+};
 
 module.exports = sendEmail;
